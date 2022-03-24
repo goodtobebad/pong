e91cdac9510eeb5750db57978aed08d94fd5940b
goodtobebad <49612579+goodtobebad@users.noreply.github.com>
2022-03-24 13:05:53 -0400

    Add files via upload

--- pong/move.h
+++ pong/move.h
@@ -6,5 +6,5 @@
 void resetBall(Ball* ball);
 void moveFirstPlayerPaddle(SDL_Event* e, SDL_Rect* paddle);
 void moveSecondPlayerPaddle(SDL_Event* e, SDL_Rect* paddle);
-bool moveBall(Ball* ball, SDL_Rect* paddle1, SDL_Rect* paddle2, int* playerOne_scored, int* playerTwo_scored);
+bool moveBall(Ball* ball, SDL_Rect* paddle1, SDL_Rect* paddle2, int *playerOne_scored, int *playerTwo_scored);
 #endif
